b7a1d2fc6f564912b46c5318d36855b9bd7bc8d5
Justin Campbell <justin@justincampbell.me>
2011-12-08 14:54:36 -0500

    Install shoulda

--- test/schlep_test.rb
+++ test/schlep_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SchlepTest < Test::Unit::TestCase
-  test "should be defined" do
+  should "be defined" do
     assert_equal Module, Schlep.class
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'rubygems'
+require 'shoulda'
 
 $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 
